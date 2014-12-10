43e5afb5a8f6317e067ab53d0f79b625cfbb1a92
phs0114 <phs0114@nate.com>
2014-12-10 14:38:25 +0900

    Default frame
    Start project

---
+++ app/src/main/java/com/jaewoo/androidopengl/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.jaewoo.androidopengl.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.jaewoo.androidopengl.R;
+
+public class MainActivity extends Activity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
