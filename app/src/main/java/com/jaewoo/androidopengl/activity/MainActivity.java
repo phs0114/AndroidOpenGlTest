a774e1f339e9340aaef6dede10c4678a32facc56
phs0114 <phs0114@nate.com>
2014-12-10 19:01:39 +0900

    14.12.10
    apply theme & Activity

--- app/src/main/java/com/jaewoo/androidopengl/activity/MainActivity.java
+++ app/src/main/java/com/jaewoo/androidopengl/activity/MainActivity.java
@@ -1,15 +1,39 @@
 package com.jaewoo.androidopengl.activity;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ListView;
 
 import com.jaewoo.androidopengl.R;
+import com.jaewoo.androidopengl.adapter.AdapterOpenGL;
 
-public class MainActivity extends Activity
-{
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+import butterknife.OnItemClick;
+
+public class MainActivity extends Activity{
+    @InjectView(R.id.main_list_menu)
+    protected ListView mMainListView;
+
+    private AdapterOpenGL mAdapterMenu;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.inject(this);
+
+        mAdapterMenu = new AdapterOpenGL(this);
+        mMainListView.setAdapter(mAdapterMenu);
+    }
+
+    @OnItemClick(R.id.main_list_menu)
+    public void onClickMenu(int position){
+        AdapterOpenGL.ITEM item = mAdapterMenu.getItem(position);
+        if(item != null){
+            Intent intent = new Intent(this,item.getActivityClass());
+            startActivity(intent);
+        }
     }
 }
